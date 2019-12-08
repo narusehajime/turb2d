ab431d7a542c767a456ea1d97d82f91ef878d2d6
narusehajime <naruse@kueps.kyoto-u.ac.jp>
2019-12-09 03:17:54 +0900

    Setup script was modified to compile cython module

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
-
-from setuptools import setup, find_packages
+import numpy as np
+from Cython.Build import cythonize
+from setuptools import setup, Extension, find_packages
 
 with open('README.rst') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
     license = f.read()
+
+ext = Extension("cip",
+                sources=["turb2d/cip.pyx"],
+                include_dirs=[np.get_include()])
 
 setup(name='turb2d',
       version='0.1.0',
@@ -17,4 +22,5 @@ setup(name='turb2d',
       url='https://github.com/narusehajime/turb2d.git',
       license=license,
       install_requires=['numpy', 'landlab', 'matplotlib', 'gdal'],
+      ext_modules=cythonize([ext]),
       packages=find_packages(exclude=('tests', 'docs')))
