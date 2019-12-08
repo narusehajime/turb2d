8443cc3be70fb0e14b949988c2d225db178ed05f
narusehajime <naruse@kueps.kyoto-u.ac.jp>
2019-12-09 00:52:37 +0900

    Debug 6 on setup.py

--- turb2d/setup.py
+++ turb2d/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-setup(cmdclass={'build_ext': build_ext},
-      ext_modules=[
-          Extension("cip", ["cip.pyx"], include_dirs=[np.get_include()])
-      ])
+ext = Extension("cip",
+                sources=["cip.pyx"],
+                include_dirs=['.', np.get_include()])
+setup(name="cip", ext_modules=cythonize([ext]))
